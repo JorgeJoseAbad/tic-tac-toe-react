cb9014b7806046cf7247e6818a2ae4af3e8445a7
Jorge Abad <jorgejoseabad@gmail.com>
2018-10-15 22:45:08 +0200

    Updates

---
+++ precache-manifest.eb95296ec3513732d5285692a3e915ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a1bc39042ecf940668",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.24a1bc39.js"
+  },
+  {
+    "revision": "c7483b5d41affe231f81",
+    "url": "/tic-tac-toe-react/static/js/main.c7483b5d.chunk.js"
+  },
+  {
+    "revision": "ba17d67f2f77432f0ec5",
+    "url": "/tic-tac-toe-react/static/js/1.ba17d67f.chunk.js"
+  },
+  {
+    "revision": "c7483b5d41affe231f81",
+    "url": "/tic-tac-toe-react/static/css/main.fb8f1656.chunk.css"
+  },
+  {
+    "revision": "6335c1d2921f8d532f29135451a0d61d",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
